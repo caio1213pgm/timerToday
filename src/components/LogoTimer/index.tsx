5bf814efefe3ee5a9aae203ec37ec10b4a911835
caio1213pgm <caiofxpgm@gmail.com>
2026-01-02 23:11:00 -0300

    feat(logo-timer): add LogoTimer component
    - Implements new LogoTimer component
    - Displays "TimerToday" with icon
    - Uses lucide-react AlarmClock icon
    - Provides dedicated styling module

---
+++ src/components/LogoTimer/index.tsx
@@ -0,0 +1,13 @@
+import { AlarmClock } from "lucide-react";
+import style from "./LogoTime.module.css";
+
+export default function LogoTimer() {
+    return (
+        <div className={style.logo}>
+            <a className={style.logoLink} href="#">
+                <AlarmClock size={64} />
+                <span>TimerToday</span>
+            </a>
+        </div>
+    );
+}
