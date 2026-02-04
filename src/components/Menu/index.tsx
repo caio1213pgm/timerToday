78125f863c348198c44a15fa6aaaacbf2900d8b8
Caio1213 <caiofxpgm@gmail.com>
2026-02-04 14:34:37 -0300

    feat: modify icons from menu

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,14 +1,14 @@
-import { Timer, TimerOff, TimerReset } from "lucide-react";
+import { HistoryIcon, HouseIcon, Settings, SunIcon } from "lucide-react";
 import NavButton from "../NavButton";
 import style from "./Menu.module.css";
 
 export default function Menu() {
   return (
     <nav className={style.navBox}>
-      <NavButton link="#" icon={<Timer />} />
-      <NavButton link="#" icon={<TimerReset />} />
-      <NavButton link="#" icon={<TimerOff />} />
-      <NavButton link="#" icon={<Timer />} />
+      <NavButton link="#" icon={<HouseIcon />} />
+      <NavButton link="#" icon={<HistoryIcon />} />
+      <NavButton link="#" icon={<Settings />} />
+      <NavButton link="#" icon={<SunIcon />} />
     </nav>
   );
 }
