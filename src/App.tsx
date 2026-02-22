6d986e57232789f06f0a10a84bb6a3bb176f202f
caio1213pgm <caiofxpgm@gmail.com>
2026-02-21 21:38:13 -0300

    feat: move components to pages directory

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,12 @@
-import BoxContainer from "./components/BoxContainer";
-import CountDown from "./components/CountDown";
-import FormAddTask from "./components/Forms/FormAddTask";
+import Home from "./pages/Home";
+
 import "./styles/global.css";
 import "./styles/theme.css";
-import DefaultTemplate from "./template/DefaultTemplate";
 
 function App() {
   return (
     <>
-      <DefaultTemplate>
-        <BoxContainer textId="count">
-          <CountDown />
-        </BoxContainer>
-        <BoxContainer textId="form">
-          <FormAddTask />
-        </BoxContainer>
-      </DefaultTemplate>
+      <Home />
     </>
   );
 }
---
+++ src/pages/Home.tsx
@@ -0,0 +1,17 @@
+import BoxContainer from "../components/BoxContainer";
+import CountDown from "../components/CountDown";
+import FormAddTask from "../components/Forms/FormAddTask";
+import DefaultTemplate from "../template/DefaultTemplate";
+
+export default function Home() {
+  return (
+    <DefaultTemplate>
+      <BoxContainer textId="count">
+        <CountDown />
+      </BoxContainer>
+      <BoxContainer textId="form">
+        <FormAddTask />
+      </BoxContainer>
+    </DefaultTemplate>
+  );
+}
