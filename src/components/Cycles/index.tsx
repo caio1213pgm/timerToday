1843854bd3214e20d56063917b0127f1926d6865
caio1213pgm <caiofxpgm@gmail.com>
2026-03-18 22:38:12 -0300

    feat: create Cycles component to visualize task types as cycle indicators.

--- src/components/Cycles/index.tsx
+++ src/components/Cycles/index.tsx
@@ -1,13 +1,33 @@
+import { useTask } from "../../hooks/useTask";
 import style from "./Cycles.module.css";
 
-export default function Cycles() {
+interface CyclesProps {
+  cicle: number;
+}
+
+export default function Cycles({ cicle }: CyclesProps) {
+  const { taskState } = useTask();
+
   return (
     <div className={style.boxCycles}>
       <span>Ciclos:</span>
       <div className={style.cycles}>
-        <span className={style.cycleWork}></span>
-        <span className={style.cycleShort}></span>
-        <span className={style.cycleLong}></span>
+        {taskState.tasks.map(({ type }) => (
+          <>
+            {type === "work" && (
+              <span className={style.cycleWork} key={type}></span>
+            )}
+            {type === "shortBreak" && (
+              <span className={style.cycleShort} key={type}></span>
+            )}
+            {type === "longBreak" && (
+              <span className={style.cycleLong} key={type}></span>
+            )}
+          </>
+        ))}
+        {/* {cicle === 8 && <span className={style.cycleLong}></span>}
+        {cicle % 2 === 0 && <span className={style.cycleShort}></span>}
+        {cicle % 2 !== 0 && <span className={style.cycleWork}></span>} */}
       </div>
     </div>
   );
