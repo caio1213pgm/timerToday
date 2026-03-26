d025da1855ed76fe9ee7256f8c558011b1522c8f
caio1213pgm <caiofxpgm@gmail.com>
2026-03-25 21:02:02 -0300

    feat: Implement task management with a new task context, cycle visualization, and reusable UI components for input and buttons.

--- src/components/Cycles/index.tsx
+++ src/components/Cycles/index.tsx
@@ -1,4 +1,7 @@
+import { useEffect } from "react";
 import { useTask } from "../../hooks/useTask";
+import { nextCycle } from "../../utils/nextCycle";
+import { nextTypeCycle } from "../../utils/nextTypeCycle";
 import style from "./Cycles.module.css";
 
 interface CyclesProps {
@@ -8,26 +11,34 @@ interface CyclesProps {
 export default function Cycles({ cicle }: CyclesProps) {
   const { taskState } = useTask();
 
+  const cyclesStates = Array.from({ length: taskState.currentCycle });
+
+  const labels = {
+    work: "Ciclo de trabalho",
+    shortBreak: "Ciclo de descanso curto",
+    longBreak: "Ciclo de descanso longo",
+  };
+
+  useEffect(() => {
+    cyclesStates.push(cicle);
+  }, [cicle]);
+
   return (
     <div className={style.boxCycles}>
       <span>Ciclos:</span>
       <div className={style.cycles}>
-        {taskState.tasks.map(({ type }) => (
-          <>
-            {type === "work" && (
-              <span className={style.cycleWork} key={type}></span>
-            )}
-            {type === "shortBreak" && (
-              <span className={style.cycleShort} key={type}></span>
-            )}
-            {type === "longBreak" && (
-              <span className={style.cycleLong} key={type}></span>
-            )}
-          </>
-        ))}
-        {/* {cicle === 8 && <span className={style.cycleLong}></span>}
-        {cicle % 2 === 0 && <span className={style.cycleShort}></span>}
-        {cicle % 2 !== 0 && <span className={style.cycleWork}></span>} */}
+        {cyclesStates.map((type, index) => {
+          const nextCicle = nextCycle(index);
+          const typeCycle = nextTypeCycle(nextCicle);
+          return (
+            <span
+              className={style[typeCycle]}
+              key={index}
+              aria-label={labels[typeCycle]}
+              title={labels[typeCycle]}
+            ></span>
+          );
+        })}
       </div>
     </div>
   );
