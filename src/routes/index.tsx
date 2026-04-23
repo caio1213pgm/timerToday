a2859722fc3219dd2f212f1f789cb37c4fbfa5b8
Caio1213 <caiofxpgm@gmail.com>
2026-04-23 13:14:21 -0300

    feat: implement about page in appRoutes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,26 @@
-import { BrowserRouter, Route, Routes } from "react-router";
+import { BrowserRouter, Route, Routes, useLocation } from "react-router";
 import Home from "../pages/Home";
+import { About } from "../pages/about/About";
+import { useEffect } from "react";
+
+function ScroolTop() {
+  const { pathname } = useLocation();
+  useEffect(() => {
+    window.scrollTo({
+      top: 0,
+    });
+  }, [pathname]);
+  return null;
+}
 
 export default function AppRoutes() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
       </Routes>
+      <ScroolTop />
     </BrowserRouter>
   );
 }
